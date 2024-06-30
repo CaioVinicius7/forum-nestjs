ae4bbe243bc5beafede2aa14bf9b63baad8e5b2a
Caio Vinícius <caio1525pereira@gmail.com>
2024-06-29 21:23:59 -0300

    refactor: :recycle: Remoção de biblioteca zod-validation-error

--- src/pipes/zod-validation.pipe.ts
+++ src/pipes/zod-validation.pipe.ts
@@ -1,6 +1,5 @@
 import { BadRequestException, PipeTransform } from "@nestjs/common";
 import { ZodError, type ZodSchema } from "zod";
-import { fromZodError } from "zod-validation-error";
 
 export class ZodValidationPipe implements PipeTransform {
   constructor(private schema: ZodSchema) {}
@@ -14,7 +13,7 @@ export class ZodValidationPipe implements PipeTransform {
         throw new BadRequestException({
           message: "Validation failed",
           statusCode: 400,
-          errors: fromZodError(error)
+          errors: error.flatten().fieldErrors
         });
       }
 
