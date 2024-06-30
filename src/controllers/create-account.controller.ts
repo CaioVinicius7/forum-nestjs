f33ec0519b0a4c1d38546df96fcc38a52e66aa14
Caio Vinícius <caio1525pereira@gmail.com>
2024-06-29 21:05:46 -0300

    feat: :lock: Gerando hash da senha

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -6,6 +6,7 @@ import {
   HttpStatus,
   Post
 } from "@nestjs/common";
+import { hash } from "bcryptjs";
 import { PrismaService } from "src/prisma/prisma.service";
 
 @Controller("/accounts")
@@ -29,11 +30,13 @@ export class CreateAccountController {
       );
     }
 
+    const hashedPassword = await hash(password, 8);
+
     await this.prisma.user.create({
       data: {
         name,
         email,
-        password
+        password: hashedPassword
       }
     });
   }
