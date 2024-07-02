0db72a9768204ed2cfa893ce202d5da890e610fc
Caio Vinícius <caio1525pereira@gmail.com>
2024-07-02 19:59:12 -0300

    chore: :wrench: Configuração inicial da autenticação JWT

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 
+import { AuthModule } from "./auth/auth.module";
 import { CreateAccountController } from "./controllers/create-account.controller";
 import { envSchema } from "./env";
 import { PrismaService } from "./prisma/prisma.service";
@@ -10,7 +11,8 @@ import { PrismaService } from "./prisma/prisma.service";
     ConfigModule.forRoot({
       validate: (env) => envSchema.parse(env),
       isGlobal: true
-    })
+    }),
+    AuthModule
   ],
   controllers: [CreateAccountController],
   providers: [PrismaService]
