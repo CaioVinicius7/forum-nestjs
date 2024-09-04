d21f25676f21cb0f1b09a28bc6930e788913a105
Caio Vinícius <caio1525pereira@gmail.com>
2024-09-04 01:18:23 -0300

    feat: :sparkles: Gerando token JWT com chave pública e privada (RS256)

---
+++ src/controllers/authenticate.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Post } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+
+@Controller("/sessions")
+export class AuthenticateController {
+  constructor(private readonly jwtService: JwtService) {}
+
+  @Post()
+  async handle() {
+    const token = this.jwtService.sign({
+      sub: "user-id"
+    });
+
+    return {
+      token
+    };
+  }
+}
