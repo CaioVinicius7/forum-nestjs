2c931f08538f7fddb3e3a5c8042b9344aed65588
Caio Vinícius <caio1525pereira@gmail.com>
2024-09-17 20:31:47 -0300

    test: :white_check_mark: Testes E2E da criação de usuários ([POST] /accounts)

--- src/controllers/create-account.controller.e2e-spec.ts
+++ src/controllers/create-account.controller.e2e-spec.ts
@@ -1,3 +1,43 @@
-test("1 plus 1", () => {
-  expect(1 + 1).toBe(2);
+import type { INestApplication } from "@nestjs/common";
+import { Test } from "@nestjs/testing";
+import request from "supertest";
+
+import { AppModule } from "@/app.module";
+import { PrismaService } from "@/prisma/prisma.service";
+
+describe("Create account (E2E)", () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule]
+    })
+
+      .compile();
+
+    app = moduleRef.createNestApplication();
+
+    prisma = moduleRef.get(PrismaService);
+
+    await app.init();
+  });
+
+  test("[POST] /accounts", async () => {
+    const response = await request(app.getHttpServer()).post("/accounts").send({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password: "123456"
+    });
+
+    expect(response.statusCode).toBe(201);
+
+    const userOnDatabase = prisma.user.findUnique({
+      where: {
+        email: "johndoe@gmail.com"
+      }
+    });
+
+    expect(userOnDatabase).toBeTruthy();
+  });
 });
