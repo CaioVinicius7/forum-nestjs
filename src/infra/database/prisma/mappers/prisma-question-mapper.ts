b810b2cba945f29ae03f4ae612a582c4b6f587b0
Caio Vinícius <caio1525pereira@gmail.com>
2024-10-17 23:36:59 -0300

    feat: :sparkles: Implementação do QuestionsRepository

--- src/infra/database/prisma/mappers/prisma-question-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-question-mapper.ts
@@ -1,4 +1,4 @@
-import type { Question as PrismaQuestion } from "@prisma/client";
+import type { Prisma, Question as PrismaQuestion } from "@prisma/client";
 
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { Question } from "@/domain/forum/enterprise/entities/question";
@@ -20,5 +20,18 @@ export class PrismaQuestionMapper {
       },
       new UniqueEntityID(raw.id)
     );
+  }
+
+  static toPrisma(question: Question): Prisma.QuestionUncheckedCreateInput {
+    return {
+      id: question.id.toString(),
+      authorId: question.authorId.toString(),
+      bestAnswerId: question.bestAnswerId?.toString(),
+      title: question.title,
+      content: question.content,
+      slug: question.slug.value,
+      createdAt: question.createdAt,
+      updatedAt: question.updatedAt
+    };
   }
 }
