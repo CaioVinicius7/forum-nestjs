2caf3a1ac803d4d4819f646d84e080833cdc3ab5
Caio Vinícius <caio1525pereira@gmail.com>
2025-02-06 12:34:40 -0300

    refactor: :fire: Remoção de método não utilizado

--- src/infra/http/controllers/create-question.controller.ts
+++ src/infra/http/controllers/create-question.controller.ts
@@ -47,13 +47,4 @@ export class CreateQuestionController {
       throw new BadRequestException();
     }
   }
-
-  private convertToSlug(title: string): string {
-    return title
-      .toLowerCase()
-      .normalize("NFD")
-      .replace(/[\u0300-\u036f]/g, "")
-      .replace(/[^\w\s-]/g, "")
-      .replace(/\s+/g, "-");
-  }
 }
