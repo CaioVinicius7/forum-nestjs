336bef1b4ddb098c8e6d71f36a1f0cbaa25f5e53
Caio Vinícius <caio1525pereira@gmail.com>
2025-03-21 11:58:20 -0300

    feat: :sparkles: Controller de busca de respostas

---
+++ src/infra/http/controllers/fetch-question-answers.controller.ts
@@ -0,0 +1,50 @@
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Param,
+  Query
+} from "@nestjs/common";
+import { z } from "zod";
+
+import { FetchQuestionAnswersUseCase } from "@/domain/forum/application/use-cases/fetch-question-answers";
+import { AnswerPresenter } from "@/infra/presenters/answer-presenter";
+
+import { ZodValidationPipe } from "../pipes/zod-validation.pipe";
+
+const pageQueryParamSchema = z
+  .string()
+  .optional()
+  .default("1")
+  .transform(Number)
+  .pipe(z.number().min(1));
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema);
+
+type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>;
+
+@Controller("/questions/:questionId/answers")
+export class FetchQuestionsAnswersController {
+  constructor(
+    private readonly fetchQuestionsAnswers: FetchQuestionAnswersUseCase
+  ) {}
+
+  @Get()
+  async handle(
+    @Query("page", queryValidationPipe) page: PageQueryParamSchema,
+    @Param("questionId") questionId: string
+  ) {
+    const result = await this.fetchQuestionsAnswers.execute({
+      page,
+      questionId
+    });
+
+    if (result.isLeft()) {
+      throw new BadRequestException();
+    }
+
+    const answers = result.value.answers;
+
+    return { answers: answers.map(AnswerPresenter.toHTTP) };
+  }
+}
